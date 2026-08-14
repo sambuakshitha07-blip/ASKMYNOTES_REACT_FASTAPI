13b6545e7eb4322b65c73b4b9de5031d0163d877
sambuakshitha13 <sambuakshitha07@gmail.com>
2026-08-14 13:02:57 +0530

    Fix deployment settings

--- backend/main.py
+++ backend/main.py
@@ -16,8 +16,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "https://askmynotes-react-fastapi-2.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
